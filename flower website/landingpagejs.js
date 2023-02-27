afa986c9a19439dcae3cd6991ced754cf1b11add
mayuShinde <mayurism183@gmail.com>
2023-02-27 15:40:43 +0530

    Firat commit

---
+++ flower website/landingpagejs.js
@@ -0,0 +1,31 @@
+function toggleMenu() {
+    const togglemenu = document.querySelector(".toggleMenu");
+    const navigation = document.querySelector(".navigation");
+    toggleMenu.classList.toggle("active");
+    navigation.classList.toggle("active");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
